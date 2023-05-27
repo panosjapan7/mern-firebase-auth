e7a7c75a56df47fa1f6c0fcbc2bd06b6ded4b6a9
panosjapan7 <panosjapan@gmail.com>
2023-05-27 09:51:10 +0200

    Sets up Firebase authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { fire } from "./fire";
+
 import "./App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  fire.auth().onAuthStateChanged((user) => {
+    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  });
+
+  console.log("logged in?", isLoggedIn);
+
   return <div className="App">Hello, World!!!</div>;
 }
 

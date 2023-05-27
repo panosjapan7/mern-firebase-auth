b94540132533043e3e38d69b910937e1b63269e2
panosjapan7 <panosjapan@gmail.com>
2023-05-27 11:06:44 +0200

    Connects to MongoDB

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,7 +15,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const phones_1 = __importDefault(require("./controllers/phones"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv_1.default.config();
+const connectMongoDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const conn = yield mongoose_1.default.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
+    catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+});
+connectMongoDB();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use("/api", phones_1.default);
--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,21 @@
 import express, { Express, Router, Request, Response } from "express";
 import phonesRouter from "./controllers/phones";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+const connectMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error: any) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+connectMongoDB();
 
 const app: Express = express();
 const port = process.env.PORT;

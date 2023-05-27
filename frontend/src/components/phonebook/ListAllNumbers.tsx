9dd36ab3550fa047f12180a1f7f618bab05dd2c0
panosjapan7 <panosjapan@gmail.com>
2023-05-27 10:18:40 +0200

    Displays phone numbers, creates link to add new number

--- frontend/src/components/phonebook/ListAllNumbers.tsx
+++ frontend/src/components/phonebook/ListAllNumbers.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ListAllNumbers = () => {
-  return <div>ListAllNumbers</div>;
+  return (
+    <div>
+      <br />
+      <Link to="/add-number">Add number</Link>
+      <h2>Phone Numbers</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Foo Bar</td>
+            <td>555-800-600</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ListAllNumbers;

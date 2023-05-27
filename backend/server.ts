95a50b861c038ba3d6736e194df1ecd6036f4a4d
panosjapan7 <panosjapan@gmail.com>
2023-05-27 10:49:45 +0200

    Uses /api route with phonesRouter controller

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Router, Request, Response } from "express";
+import phonesRouter from "./controllers/phones";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use("/api", phonesRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");

8eb15c3c676f2f15d367243d2b3391d91b530b51
panosjapan7 <panosjapan@gmail.com>
2023-05-28 08:38:41 +0200

    Makes a POST request from frontend

--- frontend/src/services/phonebookServices.ts
+++ frontend/src/services/phonebookServices.ts
@@ -1,2 +1,10 @@
-console.log("hello");
-export {};
+import axios from "axios";
+
+const url = "http://localhost:3001/api";
+export const addToPhonebook = (name: string, number: string) => {
+  const payload = {
+    name,
+    number,
+  };
+  axios.post(url, payload).then((response) => console.log(response));
+};

8910bf3ecd579e09d637539d5f37923272282f9a
panosjapan7 <panosjapan@gmail.com>
2023-05-28 08:44:42 +0200

    Creates firebase user token, sends token in headers of addToPhonebook POST

--- frontend/src/services/phonebookServices.ts
+++ frontend/src/services/phonebookServices.ts
@@ -1,10 +1,32 @@
 import axios from "axios";
+import { fire } from "../fire";
 
 const url = "http://localhost:3001/api";
-export const addToPhonebook = (name: string, number: string) => {
+
+export const addToPhonebook = async (name: string, number: string) => {
+  const header = await createFirebaseToken();
+
   const payload = {
     name,
     number,
   };
-  axios.post(url, payload).then((response) => console.log(response));
+  try {
+    const res = await axios.post(url, payload, header);
+    return res.data;
+  } catch (error) {
+    console.log("Error in addToPhoneBook: ", error);
+  }
+};
+
+const createFirebaseToken = async () => {
+  const user = fire.auth().currentUser;
+  const token = user && (await user.getIdToken());
+
+  const payloadHeader = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return payloadHeader;
 };

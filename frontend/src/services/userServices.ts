c2cebcda1aa3520007323dbf8b456e970e88c246
panosjapan7 <panosjapan@gmail.com>
2023-05-29 15:56:49 +0200

    Creates functions to POST new user on Mongo & GET all users from Mongo

---
+++ frontend/src/services/userServices.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { fire } from "../fire";
+
+const url = "http://localhost:3001/api/users";
+
+export const addUserToMongo = async (
+  uid: string | undefined,
+  email: string,
+  displayName?: string | undefined,
+  roles?: string[],
+  createdAt?: string
+) => {
+  const header = await createFirebaseToken();
+  const user = fire.auth().currentUser;
+
+  const payload = {
+    uid: user?.uid,
+    email: user?.email,
+    displayName: user?.displayName,
+    roles: ["user"],
+    createdAt: user?.metadata.creationTime,
+  };
+
+  try {
+    const res = await axios.post(url, payload, header);
+    return res.data;
+  } catch (error) {
+    console.log("Error in addUserToMongo: ", error);
+  }
+};
+
+const createFirebaseToken = async () => {
+  const user = fire.auth().currentUser;
+  const token = user && (await user.getIdToken());
+
+  const payloadHeader = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return payloadHeader;
+};
+
+export const getUsersFromMongo = async () => {
+  const header = await createFirebaseToken();
+  try {
+    const res = await axios.get(url, header);
+    return res.data;
+  } catch (error) {
+    console.log("Error in getUsersFromMongo: ", error);
+  }
+};

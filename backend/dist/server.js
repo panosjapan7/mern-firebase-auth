c45c6d58af1230c3b36fc686fb14f63050be7050
panosjapan7 <panosjapan@gmail.com>
2023-05-29 15:54:46 +0200

    Creates '/api/users' route

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -9,6 +9,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const authenticateToken_1 = __importDefault(require("./authenticateToken"));
 const connectMongoDB_1 = __importDefault(require("./mongodb/connectMongoDB"));
+const users_1 = __importDefault(require("./controllers/users"));
 dotenv_1.default.config();
 (0, connectMongoDB_1.default)();
 const app = (0, express_1.default)();
@@ -17,6 +18,7 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(authenticateToken_1.default);
 app.use("/api", phones_1.default);
+app.use("/api/users", users_1.default);
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });

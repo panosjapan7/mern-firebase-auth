ca0d47b19a83237b86d4f26c2c3023d33f4f4b77
panosjapan7 <panosjapan@gmail.com>
2023-05-29 13:49:38 +0200

    Removes console.log

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,6 @@ import Home from "./pages/Home";
 function App() {
   const { user, setUser } = useContext(UserContext);
 
-  console.log("user: ", user);
-
   return (
     <div className="App">
       <Router>

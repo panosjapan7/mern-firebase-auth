245df27feeb77bbe8ac5633d6c108b7fffca570d
panosjapan7 <panosjapan@gmail.com>
2023-05-31 14:44:21 +0200

    Deletes File

--- frontend/src/components/phonebook/ListAllNumbers.tsx
+++ frontend/src/components/phonebook/ListAllNumbers.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { getPhoneBookEntries } from "../../services/phonebookServices";
+import {
+  getPhoneBookEntries,
+  getUserPhoneBookeEntries,
+} from "../../services/phonebookServices";
 import AddNumber from "./AddNumber";
 
 interface Props {
@@ -13,19 +16,30 @@ interface Entry {
 }
 
 const ListAllNumbers: React.FC<Props> = ({ uid }) => {
-  const [entries, setEntries] = useState<Entry[]>();
+  const [entries, setEntries] = useState<Entry[]>([]);
+  const [userEntries, setUserEntries] = useState<Entry[]>([]);
 
   const fetchEntries = async () => {
     const fetchedEntries = await getPhoneBookEntries();
     setEntries(fetchedEntries);
   };
 
+  const fetchUserEntries = async () => {
+    const fetchedEntries = await getUserPhoneBookeEntries(uid);
+    setUserEntries(fetchedEntries);
+  };
+
   const handleEntryAdded = () => {
     fetchEntries();
   };
 
+  const handleUserEntryAdded = () => {
+    fetchUserEntries();
+  };
+
   useEffect(() => {
     fetchEntries();
+    fetchUserEntries();
   }, []);
 
   if (entries === undefined) {
@@ -61,7 +75,39 @@ const ListAllNumbers: React.FC<Props> = ({ uid }) => {
           </tbody>
         </table>
       </div>
-      <AddNumber uid={uid} handleEntryAdded={handleEntryAdded} />
+
+      <div>
+        <h2>This User's Phone Numbers</h2>
+
+        <table
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 5,
+          }}
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userEntries.map((entry) => (
+              <tr key={entry.id}>
+                <td>{entry.name}</td>
+                <td>{entry.number}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <AddNumber
+        uid={uid}
+        handleEntryAdded={handleEntryAdded}
+        handleUserEntryAdded={handleUserEntryAdded}
+      />
     </div>
   );
 };

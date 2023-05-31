d127f25b7037219add2ddd9d42455fb5cc1cb5a7
panosjapan7 <panosjapan@gmail.com>
2023-05-31 08:05:44 +0200

    Checks if user is logged in with firebase and renders Links conditionally

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { fire } from "../fire";
+import { useState } from "react";
+import { User } from "@firebase/auth-types";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loggedUser, setLoggedUser] = useState<User | null>(null);
+
+  fire.auth().onAuthStateChanged((user) => {
+    user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    setLoggedUser(user);
+  });
+
   const signOut = () => {
     fire.auth().signOut();
   };
@@ -25,24 +35,43 @@ const Header = () => {
           gap: 20,
         }}
       >
-        <div>
-          <Link to="/login" style={{ textDecoration: "none", color: "black" }}>
-            Login
-          </Link>
-        </div>
-        <div>
-          <Link
-            to="/register"
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            Register
-          </Link>
-        </div>
-        <div>
-          <p onClick={signOut} style={{ margin: 0, cursor: "pointer" }}>
-            Sign out
-          </p>
-        </div>
+        {!isLoggedIn && (
+          <>
+            <div>
+              <Link
+                to="/login"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Login
+              </Link>
+            </div>
+            <div>
+              <Link
+                to="/register"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Register
+              </Link>
+            </div>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <div>
+              <Link
+                to="/protected-page"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Protected Page
+              </Link>
+            </div>
+            <div>
+              <p onClick={signOut} style={{ margin: 0, cursor: "pointer" }}>
+                Sign out
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );

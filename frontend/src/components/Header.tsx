60c4ff3f196180982fa9854055a6289b68f3699f
panosjapan7 <panosjapan@gmail.com>
2023-06-02 10:01:33 +0200

    Removes unused import: User, useState

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { fire } from "../fire";
-import { useState } from "react";
-import { User } from "@firebase/auth-types";
 
 interface Props {
   isLoggedIn: boolean | null;

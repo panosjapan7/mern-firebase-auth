2d83f89b7b677371ebd922c22c7954bcd8433002
panosjapan7 <panosjapan@gmail.com>
2023-07-14 10:54:39 +0700

    Prepares to deploy on Vercel

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -27,3 +27,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+module.exports = app;
--- backend/server.ts
+++ backend/server.ts
@@ -29,3 +29,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
